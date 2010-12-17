e1f7e8bcaa6406407e6097e5197f0220b0635f29
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2010-12-17 01:31:34 -0430

    Versión Inicial

---
+++ upstream/uk/defaults/pref/all-l10n.js
@@ -0,0 +1,7 @@
+//@line 36 "/buildbot/linux_repack_31/build/releases/l10n-mozilla-1.9.2/uk/mail/all-l10n.js"
+
+//@line 38 "/buildbot/linux_repack_31/build/releases/l10n-mozilla-1.9.2/uk/mail/all-l10n.js"
+
+
+pref("general.useragent.locale", "uk");
+pref("spellchecker.dictionary", "uk");
